e0737dbda9144ad864345f3e2e6a5fe6bb413e7c
Marcin Lenkowski <marcin.lenkowski@zyagnum.com>
2024-03-10 15:09:55 +0100

    stage6 - first tests finally!

---
+++ tests/Unit/CertificateTest.php
@@ -0,0 +1,19 @@
+<?php
+
+use Lenkowski\Cert\Certificate;
+use Ramsey\Uuid\Uuid;
+
+describe('Certificate ValueObject', function () {
+    $uuid = Uuid::uuid4();
+    $cert = new Certificate(
+        'Marcin Lenkowski',
+        $uuid,
+        new \DateTimeImmutable('2021-09-01')
+    );
+
+    it('should return name', fn() => expect($cert->getName())->toBe('Marcin Lenkowski'));
+    it('should return certificate number', fn() => expect($cert->getNumber())->toBe($uuid->toString()));
+    it('should return issued at', fn () => expect($cert->getIssuedAt())->toBeInstanceOf(\DateTimeInterface::class));
+    it('should return formatted issued at', fn() => expect($cert->printIssuedAt())->toBe('2021-09-01'));
+    it('should return issuer', fn () => expect($cert->getIssuer())->toBe('Lenkowski.net'));
+});
